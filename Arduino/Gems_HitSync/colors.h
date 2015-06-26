a1f93e44bf9afe52807debd7aa37cd062675b57d
mpinner <mpinner@gmail.com>
2015-06-26 14:28:02 -0700

    encoded default colors

---
+++ Arduino/Gems_HitSync/colors.h
@@ -0,0 +1,49 @@
+
+#ifndef H_COLOR
+#define H_COLOR
+
+
+#define PIN 6
+
+static const int color[] = {
+  //strip.Color(0xF3, 0xC7, 0x49), // mustard
+  0xF1BA46, // mustard
+  0x00AEDA, //blue
+  0xA3F2CD, //tea  
+  0xE6E6FA,  //mauve
+  0xF1BA46,  // gray
+  0x508679,  //teal
+  0xBA5EDA, //0xAF6FEC, //0xD6AEDA, //lavender
+  0xF1AF62  //  orange
+//  0xF1AF92  //  peach
+  };
+  
+  static const int colorCount = 8;
+
+  
+  static const int candyColors[] = {
+    
+    
+        0xA6B0FF,
+        
+        0xE841CA,
+        
+        0xFFCD8E,
+        
+        0xDBE88B,
+        
+        0x59FF9E
+        
+        
+        
+  };
+  
+  static const int candyColorCount = 5;
+
+
+
+
+
+#endif // H_GEM
+
+
